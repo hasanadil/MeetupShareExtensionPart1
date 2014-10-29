8545ed299ccdebb1da77b391b534e7ed7d37ddca
Hasan Adil <@dispatch_hasan>
2014-10-29 13:41:13 -0400

    Initial Commit

---
+++ MeetupShareExtensionPart1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MeetupShareExtensionPart1
+//
+//  Created by Hasan Adil on 10/29/14.
+//  Copyright (c) 2014 iOSMeetup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MeetupShareExtensionPart1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MeetupShareExtensionPart1
+//
+//  Created by Hasan Adil on 10/29/14.
+//  Copyright (c) 2014 iOSMeetup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
